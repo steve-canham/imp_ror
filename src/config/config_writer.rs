24b0fd75d31e479d0007dab2f40c14c2045f1d0d
steve-canham <stevecanham@outlook.com>
2026-05-23 23:01:29 +0100

    Improving config writer

--- src/config/config_writer.rs
+++ src/config/config_writer.rs
@@ -9,20 +9,22 @@ use log::info;
 
 pub fn create_config_file() -> Result<(), AppError>
 {
-    let p1 = "                 WELCOME TO IMP_ROR               INITIAL CONFIGURATION SET UP";
-    let star_line = "**********************************************************************************";
-    let p2 = "The initial task is to set up an app_config file, to hold the details needed";
-    let p3 = "to connect to the database, and some required folder paths.";
-    let section = format!("\n\n{}\n{}\n{}\n{}\n{}\n", star_line, p1, star_line, p2, p3);
-    println!("{}", section);
+    let p = r#"
+    **********************************************************************************
+        WELCOME TO IMP_ROR               INITIAL CONFIGURATION SET UP
+    **********************************************************************************
+    The initial task is to set up an app_config file, to hold the details needed
+    to connect to the database, and some required folder paths. This 
+    "#;
+    print!("{p}");
 
+    let p = r#"
+    Section 1: DATABASE PARAMETERS
 
-    let p1 = "Section 1: DATABASE PARAMETERS";
-    let p2 = "DATABASE HOST";
-    let p3 = "Please input the name of your database host (usually the server name or IP address).";
-    let p4 = "To accept the default ('localhost') simply press enter, otherwise type the name and press enter.";
-    let section = format!("\n{}\n\n{}\n{}\n{}\n", p1, p2, p3, p4);
-    println!("{}", section);
+    DATABASE HOST
+    Please input the name of your database host (usually the server name or IP address).
+    To accept the default ('localhost') simply press enter, otherwise type the name and press enter."#;
+    println!("{p}");
  
     let mut host = user_input()?;
     let mut suffix = "";
@@ -30,45 +32,48 @@ pub fn create_config_file() -> Result<(), AppError>
         host = "localhost".to_string();
         suffix = " (= default)";
     }
-    let db_host = format!("db_host=\"{}\"", host);
-    println!("{}{}", db_host, suffix);
+    let db_host = format!(r#"db_host="{host}""#);
+    println!("{db_host}{suffix}");
     
-    let p1 = "USER NAME";
-    let p2 = "Please input the name of the user account being used to access the database.";
-    let p3 = "No default is available, type the name and press enter.";
-    let section = format!("\n{}\n{}\n{}\n", p1, p2, p3);
-    println!("{}", section);
+    let p =  r#"
+    USER NAME
+    Please input the name of the user account being used to access the database.
+    No default is available, please type the name and press enter.
+    "#;
+    println!("{p}");
 
     let user = user_input()?;
-    let db_user = format!("db_user=\"{}\"", user);
-    println!("{}", db_user);
+    let db_user = format!(r#"db_user="{user}""#);
+    println!("{db_user}");
 
-    let p1 = "USER PASSWORD";
-    let p2 = "Please input the name of the user password being used to access the database.";
-    let p3 = "No default is available, type the password and press enter.";
-    let section = format!("\n{}\n{}\n{}\n", p1, p2, p3);
-    println!("{}", section);
+    let p = r#"
+    USER PASSWORD
+    Please input the name of the user password being used to access the database.
+    No default is available, please type the password and press enter.
+    "#;
+    println!("{p}");
 
     let password = user_input()?;
-    let db_password = format!("db_password=\"{}\"", password);
-    println!("{}", db_password);
+    let db_password = format!(r#"db_password="{password}""#);
+    println!("{db_password}");
 
-    let p1 = "PORT";
-    let p2 = "Please input the port number being used to access the database.";
-    let p3 = "To accept the default ('5432') simply press enter, otherwise type the number and press enter.";
-    let section = format!("\n{}\n{}\n{}\n", p1, p2, p3);
-    println!("{}", section);
+    let p = r#"
+    PORT
+    Please input the port number being used to access the database.
+    To accept the default ('5432') simply press enter, otherwise type the number and press enter.
+    "#;
+    println!("{p}");
 
-    let mut port = -1;
+    let mut port: i32 = -1;
     let mut suffix = "";
     while port < 0 {
-        let try_port = user_input()?;
-        if try_port == "" {
+        let users_port_selection = user_input()?;
+        if users_port_selection == "" {
             port = 5432;
             suffix = " (= default)";
         }
         else {
-            port = match try_port.parse()
+            port = match users_port_selection.parse()
             {
                 Ok(n) => n,
                 Err(_) => {
@@ -78,14 +83,15 @@ pub fn create_config_file() -> Result<(), AppError>
             };
         }
     }
-    let db_port = format!("db_port=\"{}\"", port);
-    println!("{}{}", db_port, suffix);
+    let db_port = format!(r#"db_port="{port}""#);
+    println!("{db_port}{suffix}");
 
-    let p1 = "DATABASE NAME";
-    let p2 = "Please input the name of the database.";
-    let p3 = "To accept the default ('ror') simply press enter, otherwise type the name and press enter.";
-    let section = format!("\n{}\n{}\n{}\n", p1, p2, p3);
-    println!("{}", section);
+    let p = r#"
+    DATABASE NAME
+    Pease input the name of the database.
+    To accept the default ('ror') simply press enter, otherwise type the name and press enter.
+    "#;
+    print!("{p}");
 
     let mut suffix = "";
     let mut dname = user_input()?;
@@ -93,23 +99,24 @@ pub fn create_config_file() -> Result<(), AppError>
         dname = "ror".to_string();
         suffix = " (= default)";
     }
-    let db_name = format!("db_name=\"{}\"", dname);
-    println!("{}{}", db_name, suffix);
+    let db_name = format!(r#"db_name="{dname}""#);
+    println!("{db_name}{suffix}");
 
-
-    let p1 = "Section 2: FOLDERS";
-    let p2 = "DATA FOLDER";
-    let p3 = "Please input the full path of the folder where the ROR JSON source file is to be found.";
-    let p4 = "No default is available, type the path and press enter.";
-    let section = format!("\n{}\n\n{}\n{}\n{}\n", p1, p2, p3, p4);
-    println!("{}", section);
+    let p = r#"
+    Section 2: FOLDERS
+    
+    DATA FOLDER
+    Please input the full (Linux / Posix) path of the folder where the ROR JSON source file is to be found.
+    No default is available, please type the path and press enter.
+    "#;
+    println!("{p}");
 
     let mut df = "".to_string();
     while df == "".to_string() {
-        let try_df = user_input()?.replace("\\", "/");
-        if folder_exists(&PathBuf::from(&try_df))
+        let users_data_folder_selection = user_input()?;
+        if folder_exists(&PathBuf::from(&users_data_folder_selection))
         {
-            df = try_df;
+            df = users_data_folder_selection;
         }
         else
         {
@@ -117,114 +124,112 @@ pub fn create_config_file() -> Result<(), AppError>
         }
     }
     let data_folder = &df;
-    let data_folder_path = format!("data_folder_path=\"{}\"", df);
-    println!("{}", data_folder_path);
+    let data_folder_path = format!(r#"data_folder_path="{df}""#);
+    println!("{data_folder_path}");
 
-    // check is a valid path - repeat request if not
-    // change single to dounble slashes
-   
-    let p1 = "OUTPUTS FOLDER";
-    let p2 = "Please input the full path of the folder where the outputs from the program should be placed.";
-    let p3 = "To accept the default (the 'DATA FOLDER') simply press enter, otherwise type the path and press enter.";
-    let section = format!("\n{}\n{}\n{}\n", p1, p2, p3);
-    println!("{}", section);
+    let p = r#"
+    OUTPUTS FOLDER
+    Please input the full path of the (Linux / Posix) folder where the outputs from the program should be placed.
+    To accept the default (the 'DATA FOLDER') simply press enter, otherwise type the path and press enter.
+    "#;
+    println!("{p}");
 
-    let output_folder = user_input()?;
+    let users_output_folder_selection = user_input()?;
     let output_folder_path: String;
-    if output_folder == "" {
-        output_folder_path = format!("output_folder_path=\"{}\"", data_folder);
+    if users_output_folder_selection == "" {
+        output_folder_path = format!(r#"output_folder_path="{data_folder}""#);
     }
     else {
-        let op_folder = output_folder.to_string().replace("\\", "/");
-        output_folder_path = format!("output_folder_path=\"{}\"", op_folder);
+        output_folder_path = format!(r#"output_folder_path="{users_output_folder_selection}""#);
     }
-    println!("{}", output_folder_path);
+    println!("{output_folder_path}");
 
-    let p1 = "LOG FOLDER";
-    let p2 = "Please input the full path of the folder where the logs from the program should be placed.";
-    let p3 = "To accept the default (the 'DATA FOLDER') simply press enter, otherwise type the path and press enter.";
-    let section = format!("\n{}\n{}\n{}\n", p1, p2, p3);
-    println!("{}", section);
+    let p = r#"
+    LOG FOLDER
+    Please input the full path of the (Linux / Posix) folder where the logs from the program should be placed.
+    To accept the default (the 'DATA FOLDER') simply press enter, otherwise type the path and press enter.
+    "#;
+    print!("{p}");
 
-    let log_folder = user_input()?;
+    let users_log_folder_selection = user_input()?;
     let log_folder_path: String;
-    if log_folder == "" {
-        log_folder_path = format!("log_folder_path=\"{}\"", data_folder);
+    if users_log_folder_selection == "" {
+        log_folder_path = format!(r#"log_folder_path="{data_folder}""#);
     }
     else {
-        let lg_folder = log_folder.to_string().replace("\\", "/");
-        log_folder_path = format!("log_folder_path=\"{}\"", lg_folder);
+        log_folder_path = format!(r#"log_folder_path="{users_log_folder_selection}""#);
     }
     println!("{}", log_folder_path);
 
+    let p = r#"
+    Section 3: DATA PARAMETERS
+    
+    SOURCE FILE NAME
+    The source file can be provided as a command line argument, or in the configuration file, or in both.
+    NOTE that any source file name provided in the command line will over-write the value in the config file.
+    NOTE also that without a source file named in the configuration file, i.e. if enter is pressed without 
+    entering a value, a source file name will ALWAYS have to be provided in the command line.
+    "#;
+    println!("{p}");
 
-    let p1 = "Section 3: DATA PARAMETERS";
-    let p2 = "SOURCE FILE NAME";
-    let p3 = "The source file can be provided as a command line argument, or in the configuration file, or in both.";
-    let p4 = "NOTE that any source file name provided in the command line will over-write the value in the config file.";
-    let p5 = "NOTE also that without a source file named in the configuration file, i.e. if enter is pressed without ";
-    let p6 = "entering a value, a source file name will ALWAYS have to be provided in the command line.";
-    let section = format!("\n{}\n\n{}\n\n{}\n{}\n{}\n\n{}\n{}\n{}\n", p1, p2, star_line, p3, p4, p5, p6, star_line);
-    println!("{}", section);
+    let users_ppr_file_selection = user_input()?;
+    let ppr_file_name = format!(r#"ppr_file_name="{users_ppr_file_selection}""#);
+    println!("{ppr_file_name}");
 
-    let ppr_file = user_input()?;
-    let ppr_file_name = format!("ppr_file_name=\"{}\"", ppr_file);
-    println!("{}", ppr_file_name);
+    let mut data_version = format!(r#"data_version="""#); // defualts if users_ppr_file_selection is ""
+    let mut data_date = format!(r#"data_date="""#);
 
-    let mut data_version = format!("data_version=\"\"");
-    let mut data_date = format!("data_date=\"\"");
+    if users_ppr_file_selection != "" {
 
-    if ppr_file != "" {
+        let p1 = r#"
+        As you have stored a source file name in the configuration you may need to also store
+        the associated data version and date. These can be left as the defaults (empty strings)
+        if the version and date can be derived from the source file name (see documentation for the required pattern).
+        "#;
+        println!("{p1}");
 
-        let p1 = "As you have stored a source file name in the configuration you may need to also store";
-        let p2 = "the associated data version and date. These can be left as the defaults (empty strings)";
-        let p3 = "if the version and date can be derived from the source file name (see documentation for the required pattern).";
-        let section = format!("\n{}\n{}\n{}\n", p1, p2, p3);
-        println!("{}", section);
-
-
-        let p1 = "DATA VERSION";
-        let p2 = "Please input the data version, as a 'v' followed by the version number, e.g. '1.56.1'.";
-        let p3 = "To accept the default (an empty string) simply press enter, otherwise type the version and press enter.";
-        let section = format!("\n{}\n{}\n{}\n\n", p1, p2, p3);
-        println!("{}", section);
+        let p1 = r#"
+        DATA VERSION
+        Please input the data version, as a 'v' followed by the version number, e.g. '1.56.1'.
+        To accept the default (an empty string) simply press enter, otherwise type the version and press enter.
+        "#;
+        println!("{p1}");
 
         let mut suffix = "";
-        let mut d_version = "zzzz".to_string();
-        while d_version == "zzzz".to_string() {
-            let d_v = user_input()?;
-            if d_v == "".to_string()  {
-                d_version = d_v;
+        let mut d_version = "no_valid_value".to_string();
+        while d_version == "no_valid_value".to_string() {
+            let users_version_selection = user_input()?;
+            if users_version_selection == "".to_string()  {
+                d_version = "".to_string();
                 suffix = " (= default)";
             }
-            else if is_compliant_version(&d_v)? {
-                d_version = d_v;
+            else if is_compliant_version(&users_version_selection)? {   // check starts with v, and has digits and points following
+                d_version = users_version_selection;
             }
             else {
                 println!("{}", "The version entered does not conform to the pattern required - please try again");
             }
         }
-        data_version = format!("data_version=\"{}\"", d_version);
-        println!("{}{}", data_version, suffix);
+        data_version = format!(r#"data_version="{d_version}""#);
+        println!("{data_version}{suffix}");
 
-        // check starts with a v and has following digits / decimal points
-       
-        let p1 = "DATA DATE";
-        let p2 = "Please input the data date as an ISO string, yyyy-MM-dd, e.g. '2025-07-22'.";
-        let p3 = "To accept the default (an empty string) simply press enter, otherwise type the date and press enter.";
-        let section = format!("\n{}\n{}\n{}\n", p1, p2, p3);
-        println!("{}", section);
+        let p1 = r#"
+        DATA DATE
+        Please input the data date as an ISO string, yyyy-MM-dd, e.g. '2025-07-22'.
+        To accept the default (an empty string) simply press enter, otherwise type the date and press enter.
+        "#;
+        println!("{p1}");
 
         let mut suffix = "";
-        let mut d_date = "zzzz".to_string();
-        while d_date == "zzzz".to_string() {
-            let d_d = user_input()?;
-            if d_d == "".to_string() {
-                d_date = d_d;
+        let mut d_date = "no_valid_value".to_string();
+        while d_date == "no_valid_value".to_string() {
+            let users_date_iselection = user_input()?;
+            if users_date_iselection == "".to_string() {
+                d_date = "".to_string();
                 suffix = " (= default)";
             }
-            else if NaiveDate::parse_from_str(&d_d, "%Y-%m-%d").is_ok() {
-                d_date = d_d;
+            else if NaiveDate::parse_from_str(&users_date_iselection, "%Y-%m-%d").is_ok() {
+                d_date = users_date_iselection;
             }
             else {
                 println!("{}", "The date entered does not conform to the ISO pattern (yyyy-MM-dd) required - please try again");
@@ -234,43 +239,45 @@ pub fn create_config_file() -> Result<(), AppError>
         println!("{}{}", data_version, suffix);
     }
 
-
-    let database_section = format!("[database]\n{}\n{}\n{}\n{}\n{}\n", db_host, db_user, db_password, db_port, db_name);
-    let folders_section = format!("[folders]\n{}\n{}\n{}\n", data_folder_path, output_folder_path, log_folder_path);
     let data_section = format!("[data]\n{}\n{}\n{}\n", ppr_file_name, data_version, data_date);
+    let folders_section = format!("[folders]\n{}\n{}\n{}\n", data_folder_path, output_folder_path, log_folder_path);
+    let database_section = format!("[database]\n{}\n{}\n{}\n{}\n{}\n", db_host, db_user, db_password, db_port, db_name);
     let config_string = format!("\n{}\n\n{}\n\n{}\n", data_section, folders_section, database_section);
 
     let mut file = File::create("./app_config.toml")     // creates new or truncates existing
         .map_err(|e| AppError::IoWriteErrorWithPath( e, PathBuf::from("./app_config.toml")))?;
-
-    // Write to the file
     
-    file.write_all(config_string.as_bytes())
+    file.write_all(config_string.as_bytes())             // Write to the file
         .map_err(|e| AppError::IoWriteErrorWithPath( e, PathBuf::from("./app_config.toml")))?;
 
     info!("Configuration file creation completed");
-
     Ok(())
 }
 
 
 fn user_input() -> Result<String, AppError> {
     print!(">>");
-    io::stdout().flush().unwrap(); 
+    io::stdout().flush().unwrap();        // ensure >> prompt is shown
+    let mut input = String::new();        // establish buffer
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => Ok(input.trim().to_string()),      // Ok value is number of bytes read (as usize)
+        Err(e) => Err(AppError::UserInputError(e)),
+    }
+
+    /* 
     let mut input = String::new();
     io::stdin().read_line(&mut input)
         .map_err(|e| AppError::UserInputError(e))?;
     Ok(input.trim().to_string())
+    */
 }
 
 
 fn folder_exists(folder_name: &PathBuf) -> bool {
-    let res = match folder_name.try_exists() {
+    match folder_name.try_exists() {
         Ok(true) => true,
-        Ok(false) => false, 
-        Err(_e) => false,           
-    };
-    res
+        _ => false, 
+    }
 }
 
 
