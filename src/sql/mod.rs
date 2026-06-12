1dcb41226e9dcecb0b63c7048ed23f99cd2c6d95
steve-canham <stevecanham@outlook.com>
2026-06-12 19:32:54 +0100

    Re-integrate sql code weithin rust source

---
+++ src/sql/mod.rs
@@ -0,0 +1,8 @@
+pub mod create_countries_table;
+pub mod create_lang_codes_table;
+pub mod create_scripts_table;
+
+pub mod create_lup_tables;
+pub mod create_ppr_tables;
+pub mod create_src_tables;
+pub mod create_smm_tables;
