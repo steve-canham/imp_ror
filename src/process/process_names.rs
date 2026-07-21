adfdc124a426f408053319bb350e02ee55c20db9
steve-canham <stevecanham@outlook.com>
2026-07-21 09:18:22 +0100

    name processing bewing mapped out

--- src/process/process_names.rs
+++ src/process/process_names.rs
@@ -4,24 +4,199 @@ use crate::AppError;
 
 pub async fn clean_names1 (pool: &Pool<Postgres>) -> Result<(), AppError> {
 
-    // replace some silly things
+    // remnove invisible characters
+    
+    remove_unicode_char("200B", "zero width space", pool).await?;
+    remove_unicode_char("200C", "zero width no join", pool).await?;
+    remove_unicode_char("200D", "zero width join",pool).await?;
+    remove_unicode_char("200E", "left-to-right mark", pool).await?;
+    remove_unicode_char("200F", "right-to-left mark", pool).await?;
+    remove_unicode_char("2060", "word joiner", pool).await?;
+    remove_unicode_char("FEFF", "byte order mark", pool).await?;
+    remove_unicode_char("00AD", "soft hyphen", pool).await?;
 
-    let n = replace_chars("''''", "\"", pool).await?;
-    info!("Paired single apostrophes replaced by double quotes in {n} records");
-   
+    // replace possible 'odd' spaces (though don't seem to occur)
+    
+    replace_unicode_char("00A0", "non-breaking space", " ", pool).await?;  
+    replace_unicode_char("2002", "m space", " ", pool).await?;  
+    replace_unicode_char("2003", "n space", " ", pool).await?;  
+    replace_unicode_char("2008", "punctuation space", " ", pool).await?;  
+    replace_unicode_char("3000", "ideographic space", " ", pool).await?;  
+
+    // standardise hyphens
+
+    replace_unicode_char("2010", "non ascii hyphen", "-", pool).await?;  
+    replace_unicode_char("2011", "non-breaking hyphen", "-", pool).await?;  
+    replace_unicode_char("2012", "figure dash", "-", pool).await?;  
+    replace_unicode_char("2013", "n dash", "-", pool).await?;  
+    replace_unicode_char("2014", "m dash", "-", pool).await?;  
+    replace_unicode_char("2015", "horizontal bar", "-", pool).await?;  
+
+    // consider square brackets
+    // ?? considr ampoersands
+    // 
+    // Do double spaces to single at end?
+    // info!("{} double spaces replaced by single in names to match", replace_in_names("  ", " ", pool).await?);
+    
+    
+    // repair or remove some very specific oddities
+      
     let n = replace_chars("[править | править вики-текст]", "", pool).await?;
     info!("'[%править | править вики-текст]', translated as 'edit | edit wiki-text' removed in {n} records");
     
     let n = replace_chars("[ Citation needed | edit wiki text ]", "", pool).await?;
     info!("'[ Citation needed | edit wiki text ]', removed in {n} records");
 
-    
-    
+    let n = replace_chars(" (Rybářství Litomyšl)", "", pool).await?;
+    info!("Spurious repeated text removed in {n} records");
 
+    let n = replace_chars("?>", "->", pool).await?;
+    info!("Incorrect arrow formula replaced in {n} records");
+
+    /* to add 
+    update src.names set value = replace(value, '[', '') where value like '%['
+    update src.names set value = replace(value, ';', '') where value like '%;'
+    
+    update src.names set value = translate(value, '[]', '')
+    where orig_value like '%]' and orig_value like '[%'
+    -- no equivalent gfor paranthese or curly btrackets
+    
+    update src.names 
+    set value = replace(value, '[', '') 
+    where value like '%[%' and value not like '%]%'
+    
+    update src.names 
+    set value = replace(value, ']', '') 
+    where value like '%]%' and value not like '%[%'
+    */
     // Apostrophes
-   
-    info!("{} apostrophes in names, to begin with", apos_num(pool).await?);
 
+    //  First put all double quotes and equivalents as straight double quotes
+    // (necessary to correct pre-existing errors)
+    
+    replace_quotes("“", "\"", pool).await?;
+    replace_quotes("”", "\"", pool).await?;
+    replace_quotes("«", "\"", pool).await?;
+    replace_quotes("»", "\"", pool).await?;
+    replace_quotes("„", "\"", pool).await?;
+    replace_quotes("''''", "\"", pool).await?;
+
+    /*
+     * -- do before above
+     * update src.names 
+     set value = replace(value, ',,', '„')
+     where value like '%,,%'
+     */
+
+    // need to add here repolapcing two commas by the low r quotes
+    
+    info!("{} names with double quotes, to begin with", double_quotes_num(pool).await?);
+
+    // replace double doubles with single "" -> " (2 recs)
+    // consider those records (2) with 5 "
+    // In both cases drop the 5th " to make it records with 4 "
+    // update src.names 
+    // set value = trim(regexp_replace(value, '"', '', 1, 5))
+    // where length(value) - length(replace(value, '"', '')) = 5
+
+    // consider those records (32) with 3 "
+    // Which one to drop will depend on specific record - selct by id
+
+    /*
+    update src.names 
+    set value = trim(regexp_replace(value, '"', '', 1, 1))
+    where length(value) - length(replace(value, '"', '')) = 3
+    and id in('019j1v294', '01hprsv49', '01mp7gg57', '01vd5cb71', '020whct63', '028mtfb17', '02b47v767', '03dx8n755', '03q57f308', '03qc6zh37' , '03wn3aq07', '049j4jr36', '04a7dp661', '057tmwv53', '05kzawq90', '05pkv9t98', '05q23ne91', '05svms055')
+    
+    update src.names 
+    set value = trim(regexp_replace(value, '"', '', 1, 3))
+    where length(value) - length(replace(value, '"', '')) = 3
+    and id in ('00aa7ab77', '00kysjz64', '00qbdg904', '00wsvb073', '013fj3d42', '033z59547', '03b0cj417', '03xdgrg08', '05pc7fv53')
+
+    do before to make sure all hebrew names identified (they are at present)
+    update src.names
+    set lang = 'he' 
+    where  value ~ '[\u0590-\u05FF]'
+    and lang <> 'he'
+
+    change a double quote to gershayim (u05F4)
+    if it is the only doouble quoyte in the name
+
+    update src.names
+    set value = replace(value, '"', U&'\05F4')
+    where lang = 'he'
+    and length(value) - length(replace(value, '"', '')) = 1
+   
+    
+    The geresh 〈׳〉, is the Hebrew equivalent of a period in abbreviations (e.g. abbrev.), in addition to being attached to Hebrew letters to indicate sounds like soft g [dʒ] and ch [tʃ] in foreign names such as Charles (צ׳ארלס‎) and Jake (ג׳ייק‎). The gershayim 〈״〉, is a Hebrew symbol indicating that a sequence of characters is an acronym, and is placed before the last character of the word. Owing to a Hebrew keyboard's having neither a geresh nor gershayim, they are usually replaced online with, respectively, the visually similar apostrophe 〈'〉 and quotation mark 〈"⟩. The quotation mark and apostrophe are higher than the geresh and gershayim: where the latter are placed level with the top of Hebrew letters, the apostrophe and quotation marks are above them.
+
+    Then can consider names with just a single doble quote
+    In many cases add an additional quote to the end, but not in all
+
+    
+    -- put in front
+    update src.names
+    set value = '"'||value
+    where id in ('00a9b0g29', '00vrtwn56', '01g7a7y43', '03mgprp21', '052q58629', '05bpnjz66')
+    and length(value) - length(replace(value, '"', '')) = 1
+    
+    -- lose
+    update src.names
+    set value = replace(value, '"', '')
+    where id in ('04cnfv189')
+    and length(value) - length(replace(value, '"', '')) = 1
+    
+    -- put behind
+    update src.names
+    set value = value||'"'
+    where length(value) - length(replace(value, '"', '')) = 1
+    
+    
+    Three more specific oddities
+
+    update src.names set value = 'Polemikí Aeroporía'
+    where value = 'Polemikí Aeroporía, literally "Military Aviation"'
+    
+    update src.names set value = 'Public Komatsu University'
+    where value = 'literally Public Komatsu University'
+    
+    update src.names set value = replace(value, '... ', '')
+    where value like '%... %'
+
+    -- Finally change all the paired double quotes to 'proper' 66 -- 99 quotes
+    
+    update src.names 
+    set value = regexp_replace(value, '"(.*)"(.*)"(.*)"', '“\1”\2“\3”') 
+    where length(value) - length(replace(value, '"', '')) = 4
+    
+    update src.names 
+    set value = regexp_replace(value, '"(.*)"', '“\1”') 
+    where length(value) - length(replace(value, '"', '')) = 2
+    
+    // ???? put left and right quote choices in the config file...
+    // US pattern is the default but others can ber used...
+    
+    */
+
+    
+    // Consider names with paired double quotes
+    // The problem is that several have three double quotes, a pair and an 'odd' one
+
+    
+
+
+    
+    //  Then put all single quotes and equivalents as straight apostrophes
+    // (necessary to correct pre-existing errors)
+    
+    replace_quotes("‘", "''", pool).await?;
+    replace_quotes("’", "''", pool).await?;
+    
+    info!("{} names with apostrophes, to begin with", apos_num(pool).await?);
+
+
+    
+    
     // The Uzbek language includes a single left quote (though this seems to be being phased out now)
     
     let ch_type = format!("Uzbek apostrophe replaced by ‘ left single quote (usually O‘)");
@@ -147,7 +322,7 @@ pub async fn clean_names1 (pool: &Pool<Postgres>) -> Result<(), AppError> {
     info!("(Ya'an) temporarily replaced by (Ya^an) in {n} records");
     
     
-    info!("{} apostrophes in names after processing", apos_num(pool).await?);
+    info!("{} names with apostrophes after processing", apos_num(pool).await?);
     info!("");
 
     // Most of the remaining apostrophes uses to indicate syllable boundaries 
@@ -182,20 +357,43 @@ async fn remove_chars(char: &str, pool: &Pool<Postgres>) -> Result<u64, AppError
 
     Ok(res.rows_affected())
 }
-
-
-async fn remove_unicode_char(unicode_char: &str, pool: &Pool<Postgres>) -> Result<u64, AppError> {
-
-    let sql  = format!(r#"update src.names
-            set name = trim(replace(value, U&'\{unicode_char}', ''))
-            where value like U&'%\{unicode_char}%'; "#);
-
-    let res = sqlx::query(&sql).execute(pool).await
-    .map_err(|e| AppError::SqlxError(e, sql))?;
-
-    Ok(res.rows_affected())
-}
 */
+
+async fn remove_unicode_char(unicode_char: &str, char_description: &str, pool: &Pool<Postgres>) -> Result<(), AppError> {
+
+    let ch_type = format!("(\\u{unicode_char}, {char_description}) removed");
+    let sql  = format!(r#"update src.names
+            set value = trim(replace(value, U&'\{unicode_char}', '')),
+            changed = true,
+            change_type = 
+                case when change_type is null then '{ch_type}'
+                else change_type||', '||'{ch_type}'
+            end
+            where value like U&'%\{unicode_char}%'; "#);
+     
+    let n = sqlx::query(&sql).execute(pool).await
+    .map_err(|e| AppError::SqlxError(e, sql))?.rows_affected();
+
+    if n > 0 {
+        info!("{char_description} characters removed from {n} records");
+    }
+
+    Ok(())
+}
+
+
+async fn replace_quotes(chars: &str, replacement: &str, pool: &Pool<Postgres>) -> Result<u64, AppError> {
+   
+    let sql  = format!(r#"update src.names
+            set value = replace(value, '{chars}', '{replacement}'),
+            where value like '%{chars}%' "#);
+
+    let n = sqlx::query(&sql).execute(pool).await
+    .map_err(|e| AppError::SqlxError(e, sql))?.rows_affected();
+
+    Ok(n)
+}
+
 
 async fn replace_chars(chars: &str, replacement: &str, pool: &Pool<Postgres>) -> Result<u64, AppError> {
 
@@ -215,24 +413,45 @@ async fn replace_chars(chars: &str, replacement: &str, pool: &Pool<Postgres>) ->
     Ok(res.rows_affected())
 }
 
-/* 
-async fn replace_unicode_char(unicode_char: &str, replacement: &str, pool: &Pool<Postgres>) -> Result<u64, AppError> {
 
+async fn replace_unicode_char(unicode_char: &str, char_description: &str, 
+    replacement: &str, pool: &Pool<Postgres>) -> Result<(), AppError> {
+
+    let ch_type = format!("(\\u{unicode_char}, {char_description}) replaced by ({replacement})");
     let sql  = format!(r#"update src.names
-            set value = replace(name, U&'\{unicode_char}', '{replacement}')
+            set value = replace(name, U&'\{unicode_char}', '{replacement}'),
+            changed = true,
+            change_type = 
+                case when change_type is null then '{ch_type}'
+                else change_type||', '||'{ch_type}'
+            end
             where value like U&'%\{unicode_char}%'; "#);
 
-    let res = sqlx::query(&sql).execute(pool).await
-    .map_err(|e| AppError::SqlxError(e, sql))?;
+    let n = sqlx::query(&sql).execute(pool).await
+    .map_err(|e| AppError::SqlxError(e, sql))?.rows_affected();
 
-    Ok(res.rows_affected())
+    if n > 0 {
+        info!("{char_description} characters replaced by ({replacement}) in {n} records");
+    }
+
+    Ok(())
 }
-*/
+
 
 async fn apos_num(pool: &Pool<Postgres>) -> Result<i64, AppError> {
 
     let sql  = r#"select count(*) from src.names 
     where value like '%''%'"#;
+
+    let r: i64 = sqlx::query_scalar(sql).fetch_one(pool).await
+        .map_err(|e| AppError::SqlxError(e, sql.to_string()))?;
+    Ok(r)
+}
+
+async fn double_quotes_num(pool: &Pool<Postgres>) -> Result<i64, AppError> {
+
+    let sql  = r#"select count(*) from src.names 
+    where value like '%"%'"#;
 
     let r: i64 = sqlx::query_scalar(sql).fetch_one(pool).await
         .map_err(|e| AppError::SqlxError(e, sql.to_string()))?;
